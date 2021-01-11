4d18ec23e3453846aa38f2183d010da30df9f876
Thomas <thomasmesfin260@gmail.com>
2021-01-11 15:49:29 +0300

    pushed to heroku

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'..','public')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public/index.html'))
+})
+
+app.listen(process.env.PORT=3000,()=>{
+    console.log('server is runing!')
+})
--- src/action/expenses.js
+++ src/action/expenses.js
@@ -1,4 +1,4 @@
-import {v4 as uuid} from 'uuid'
+import {v4 as uuid} from 'uuid';
 
 export const addExpense = expense=>({
     type:"ADD_EXPENSE",
