fae1b6e134532ac7a73e54bf10d4db22e51465dc
Thomas <thomasmesfin260@gmail.com>
2021-01-11 18:45:39 +0300

    fixed date and  amount display

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import numeral from 'numeral';
+import moment from 'moment';
 
 import filterExpenses from '../selectors/filterExpenses'
 import { removeExpense } from '../action/expenses';
@@ -14,7 +16,7 @@ export const ExpenseList =  props => (
                 return <div key={el.id}>
                     <Link to={`/edit/${el.id}`}>
                         <h2>{el.description}</h2>
-                        <p>Amount: {el.amount}  createdAt: {el.createdAt} </p>
+                        <p>Amount: {numeral(el.amount).format('0,0.00')} ETB  createdAt: {moment(el.createdAt).format("LL")} </p>
                     </Link>
                     <button onClick={()=>props.dispatch(removeExpense(el.id))}>Remove</button>
                 </div>
